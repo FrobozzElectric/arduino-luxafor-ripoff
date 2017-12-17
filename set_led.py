ec73846fa7df7b80559070e702020796e4ebccc9
FrobozzElectric <gpfau7@gmail.com>
2017-12-17 10:45:34 -0500

    documentation

--- set_led.py
+++ set_led.py
@@ -4,6 +4,25 @@ import sys
 import time
 from serial import Serial
 
+usage = """Usage:
+
+%s <arduino port> <color>
+
+Example arduino port: /dev/ttyACM1
+
+Example colors:
+
+    000 = off
+    001 = red
+    255 = green
+    511 = blue
+    767 = red again
+""" % (sys.argv[0])
+
+if len(sys.argv) < 2:
+    print(usage)
+    sys.exit(1)
+
 port = str(sys.argv[1])
 arduino = Serial(port, 9600, timeout=.1)
 
